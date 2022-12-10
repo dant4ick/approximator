a5f930b0591aab36da91739b4aefea9d93f28a56
LyoshaGodX <alex0.oklem@gmail.com>
2022-12-11 01:56:53 +0300

    add: euler for 3 equation system

--- src/differentiation/euler.js
+++ src/differentiation/euler.js
@@ -1,6 +1,5 @@
 const math = require('mathjs');
 
-
 function euler(func, x0, localDot, y0, splits) {
   let step = (localDot - x0) / splits;
   let x = x0;
@@ -39,4 +38,39 @@ function euler2(exp1, exp2, x0, localDot, y0, dy0, splits) {
   return z;
 }
 
-console.log(euler2('z', '-2*z - y + x', 0, 1, 1, 0, 5))
+function euler3 (exp1, exp2, exp3, t0, localDot, x0, y0, z0, splits) {
+
+  let step = (localDot - t0) / splits;
+
+  let t;
+  let x;
+  let y;
+  let z;
+
+  let tBuff = t0;
+  let xBuff = x0;
+  let yBuff = y0;
+  let zBuff = z0;
+
+  let result = [{'t': tBuff, 'x': xBuff, 'y': yBuff, 'z': zBuff}]
+
+  for (let iter = 0; iter < splits; iter++) {
+
+    t = tBuff + step
+    x = xBuff + step * math.evaluate(exp1, {t: tBuff, x: xBuff, y: yBuff, z: zBuff});
+    y = yBuff + step * math.evaluate(exp2, {t: tBuff, x: xBuff, y: yBuff, z: zBuff});
+    z = zBuff + step * math.evaluate(exp3, {t: tBuff, x: xBuff, y: yBuff, z: zBuff});
+
+    tBuff = t;
+    xBuff = x;
+    yBuff = y;
+    zBuff = z;
+
+    result.push({'t': t, 'x': x, 'y': y, 'z': z})
+  }
+
+  return result;
+}
+
+
+console.log(euler3('-2x + 5z', 'sin(t-1)x - y + 3z', '-x + 2z', 0, 0.3, 2, 1, 1, 10))
