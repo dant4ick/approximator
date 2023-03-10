2bf30e2e274c9b1e0ad1aeb79c4b4860b1bea5fb
vrnnncl <97320132+vrnnncl@users.noreply.github.com>
2023-03-10 10:49:03 +0300

    add: Gauss&Ass. methods to solve systems of linear equations

---
+++ src/matrix/MatrixHandler.js
@@ -0,0 +1,158 @@
+class MatrixHandler {
+  constructor() {
+    this.resultsSection = document.getElementById('results');
+    this.solveButton = document.getElementById('solve');
+
+    this.methodNames = document.getElementsByName('method-name');
+
+    this.matrixInputElement = document.getElementById('matrix');
+    this.matrixSize = document.getElementById('size');
+
+    this.clearButton = document.getElementById('clear');
+    this.plusButton = document.getElementById('plus');
+    this.minusButton = document.getElementById('minus');
+
+    this.plusButton.addEventListener('click', () => this.updateMatrixInput(1));
+    this.minusButton.addEventListener('click',
+        () => this.updateMatrixInput(-1));
+    this.clearButton.addEventListener('click', () => this.clearMatrixInputs());
+
+    this.solveButton.addEventListener('click',
+        () => this.displayNewElem());
+  }
+
+  updateMatrixInput(changeBy) {
+    const newMatrixSize = parseInt(this.matrixSize.innerText) + changeBy;
+
+    if (newMatrixSize < 2) { return; }
+
+    this.matrixSize.innerText = newMatrixSize;
+
+    if (changeBy > 0) {
+      const newRow = document.createElement('div');
+      newRow.setAttribute('id', `r${newMatrixSize - 1}`);
+      newRow.classList.add('input-group');
+
+      const rows = this.getMatrixInputRows();
+      rows.forEach(function(row) {
+        const newCell = document.createElement('input');
+        newCell.setAttribute('type', 'text');
+        newCell.setAttribute('id', `${row.id}-c${newMatrixSize}`);
+        newCell.classList.add('form-control');
+        newCell.setAttribute('placeholder', '0');
+        row.append(newCell);
+      });
+
+      for (let i = 0; i <= newMatrixSize; i++) {
+        const newCell = document.createElement('input');
+        newCell.setAttribute('type', 'text');
+        newCell.setAttribute('id', `r${newMatrixSize - 1}-c${i}`);
+        newCell.classList.add('form-control');
+        newCell.setAttribute('placeholder', '0');
+        newRow.append(newCell);
+      }
+      this.matrixInputElement.append(newRow);
+    } else {
+      this.matrixInputElement.lastElementChild.remove();
+
+      const rows = this.getMatrixInputRows();
+      rows.forEach(function(row) {
+        row.lastElementChild.remove();
+      });
+    }
+  }
+
+  getMatrixInputRows() {
+    return this.matrixInputElement.querySelectorAll('div[id^="r"]');
+  }
+
+  getMethodName() {
+    for (let btn in this.methodNames) {
+      if (this.methodNames[btn].checked) {
+        return this.methodNames[btn].value;
+      }
+    }
+  }
+
+  clearMatrixInputs() {
+    const rows = this.getMatrixInputRows();
+    rows.forEach(function(row) {
+      const cells = row.querySelectorAll('input[id^="r"]');
+      cells.forEach(function(cell) {
+        cell.value = '';
+      });
+    });
+  }
+
+  getMatrixInputValues() {
+    let matrix = [];
+    const rows = this.getMatrixInputRows();
+    rows.forEach(function(row) {
+      let rowValues = [];
+      const cells = row.querySelectorAll('input[id^="r"]');
+      cells.forEach(function(cell) {
+        const value = parseFloat(cell.value);
+        if (isNaN(value)) {
+          rowValues.push(0);
+        } else {
+          rowValues.push(value);
+        }
+      });
+      matrix.push(rowValues);
+    });
+    return matrix;
+  }
+
+  updateTex() {
+    MathJax.typeset();
+  }
+
+  matrixToTex(matrix) {
+    return `\\begin{matrix}${matrix.map(row => {return row.join(' & ');}).
+        join('\\\\')}\\end{matrix}`;
+  }
+
+  linearEquationsToTex(coefficients, results) {
+    return `\\begin{pmatrix}${this.matrixToTex(
+        coefficients)}\\ \\Bigg| \\ ${this.matrixToTex(results)}\\end{pmatrix}`;
+  }
+
+  displayNewElem() {
+    const resultContainer = document.createElement('div');
+    resultContainer.classList.add('container', 'border', 'rounded', 'mb-3');
+
+    const inputValues = this.getMatrixInputValues();
+
+    const linearEquations = matrixToLinearEquations(inputValues);
+
+    const methodName = this.getMethodName();
+    let result;
+
+    switch (methodName) {
+      case 'gauss':
+        result = gauss(inputValues);
+        resultContainer.innerText = '$$\\mathtt{Метод\\ Гаусса}$$';
+        break;
+      case 'optimal':
+        result = gaussOptimalExclusion(inputValues);
+        resultContainer.innerText = '$$\\mathtt{Метод\\ Гаусса\\ (метод\\ оптимального\\ исключения)}$$';
+        break;
+      case 'jordan':
+        result = gaussJordan(inputValues);
+        resultContainer.innerText = '$$\\mathtt{Метод\\ Гаусса-Жордана}$$';
+        break;
+    }
+
+    const resLinearEquations = matrixToLinearEquations(result);
+
+    resultContainer.innerText += `$$\\tilde{A}=${this.linearEquationsToTex(
+        linearEquations.A, linearEquations.B)}\\sim${this.linearEquationsToTex(
+        resLinearEquations.A, resLinearEquations.B)}$$`;
+    resultContainer.innerText += `$$X=\\left(${this.matrixToTex(
+        solveLinearEquations(resLinearEquations.A,
+            resLinearEquations.B))}\\right)$$`;
+
+    this.resultsSection.prepend(resultContainer);
+    this.updateTex();
+  }
+}
---
+++ src/matrix/gauss.js
@@ -0,0 +1,73 @@
+function gauss(matrix) {
+  for (let row in matrix) {
+    row = parseInt(row);
+
+    const normalMultiplier = 1 / matrix[row][row];
+    matrix[row] = matrix[row].map(cell => {
+      return math.round(cell * normalMultiplier, 10);
+    });
+
+    for (let i = row + 1; i < matrix.length; i++) {
+      matrix[i] = matrix[i].map((cell, index) => {
+        return cell - (matrix[row][index] * matrix[i][row]);
+      });
+    }
+  }
+  return matrix.map(cell => {return math.round(cell, 10);});
+}
+
+function gaussOptimalExclusion(matrix) {
+  for (let row in matrix) {
+    row = parseInt(row);
+
+    for (let i = row + 1; i < matrix.length; i++) {
+      const multiplier = -matrix[i][row] / matrix[row][row];
+
+      matrix[i] = matrix[i].map((cell, index) => {
+        return cell + (matrix[row][index] * multiplier);
+      });
+    }
+  }
+  return matrix.map(cell => {return math.round(cell, 10);});
+}
+
+function gaussJordan(matrix) {
+  matrix = gauss(matrix);
+  for (let row = matrix.length - 1; row >= 0; row--) {
+    for (let i = row - 1; i >= 0; i--) {
+      matrix[i] = matrix[i].map((cell, index) => {
+        return cell - (matrix[row][index] * matrix[i][row]);
+      });
+    }
+  }
+  return matrix.map(cell => {return math.round(cell, 10);});
+}
+
+function matrixToLinearEquations(matrix) {
+  matrix = math.matrix(matrix);
+
+  const size = matrix.size();
+
+  let A = math.column(matrix, 0);
+  for (let i = 1; i < size[1] - 1; i++) {
+    A = math.concat(A, math.column(matrix, i));
+  }
+
+  let B = math.column(matrix, size[1] - 1);
+
+  return {A: A.toArray(), B: B.toArray()};
+}
+
+function solveLinearEquations(coefficients, results) {
+  for (let i = results.length - 1; i >= 0; i--) {
+    const accumulatedSum = coefficients[i].reduce(
+        function(currentSum, currentNumber, currentIndex) {
+          if (currentIndex === i) { return 0; }
+          return currentSum + (currentNumber * results[currentIndex][0]);
+        }, 0);
+
+    results[i][0] = math.round(
+        (results[i][0] - accumulatedSum) / coefficients[i][i], 10);
+  }
+  return results;
+}
